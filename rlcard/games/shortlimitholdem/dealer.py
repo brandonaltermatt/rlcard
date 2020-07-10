de48b999f9fe616e5254c3e843f728ccaae5127e
jake-bickle <bickle.jake@gmail.com>
2020-07-10 13:24:13 -0700

    Remove redundent copy/pasted code in ShortLimitHoldemDealer by making it a subclass of LimitHoldem's dealer

--- rlcard/games/shortlimitholdem/dealer.py
+++ rlcard/games/shortlimitholdem/dealer.py
@@ -1,24 +1,10 @@
+from rlcard.games.limitholdem import Dealer
 from rlcard.utils.utils import init_short_deck
 
-class ShortLimitHoldemDealer:
+class ShortLimitHoldemDealer(Dealer):
     def __init__(self, np_random):
         ''' Initialize a limitholdem dealer class
         '''
-        self.np_random = np_random
+        super().__init__(np_random)
         self.deck = init_short_deck()
         self.shuffle()
-        self.pot = 0
-
-    def shuffle(self):
-        ''' Shuffle the deck
-        '''
-        self.np_random.shuffle(self.deck)
-
-    def deal_card(self):
-        ''' Deal one card from the deck
-
-        Returns:
-            (Card): The drawn card from the deck
-        '''
-        return self.deck.pop()
-
