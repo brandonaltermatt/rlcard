9ac8a1310e44ea27c3f51d711232cef1f693a6e6
jake-bickle <bickle.jake@gmail.com>
2020-07-10 13:03:59 -0700

    Add test_ace_is_high_card

--- tests/utils/test_shortlimitholdem_utils.py
+++ tests/utils/test_shortlimitholdem_utils.py
@@ -1,8 +1,17 @@
 import unittest
 
-from rlcard.games.shortlimitholdem.utils import ShortHand as ShortHand
+from rlcard.games.shortlimitholdem.utils import ShortHand
 
 class TestShortLimitHoldemUtils(unittest.TestCase):
+    ''' ShortLimitHoldem is not much different from regular LimitHoldem. However, in LimitHoldem,
+    aces only count as a high card, while they count as a low and high card in Short.
+    Because of this, the majority of these tests make sure this works.
+    '''
+    def test_ace_is_high_card(self):
+        s = ShortHand(['HA', 'HT', 'H7', 'S8', 'S9', 'SK', 'SQ'])
+        s.evaluateHand()
+        self.assertEqual(s.category, 1)
+
     def test_has_ace_pair(self):
         s = ShortHand(['HA', 'HT', 'H7', 'S8', 'S9', 'SK', 'SA'])
         s.evaluateHand()
