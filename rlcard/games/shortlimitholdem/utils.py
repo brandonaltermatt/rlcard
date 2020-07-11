46dcfdd71831727fc8e25ca4c5f11da1260d9b16
jake-bickle <bickle.jake@gmail.com>
2020-07-11 11:26:19 -0700

    Refactored ShortHand:evaluateHand() to be much simpler

--- rlcard/games/shortlimitholdem/utils.py
+++ rlcard/games/shortlimitholdem/utils.py
@@ -13,46 +13,9 @@ class ShortHand(Hand):
         Evaluate all the seven cards, get the best combination catagory
         And pick the best five cards (for comparing in case 2 hands have the same Category) .
         """
-        if len(self.all_cards) != 7:
-            raise Exception(
-                "There must be 7 cards in a short hand, quit evaluation now! ")
-
-        self._sort_cards()
-        self.cards_by_rank, self.product = self._getcards_by_rank(
-            self.all_cards)
-
-        if self._has_straight_flush():
-            self.category = 9
-            #Straight Flush
-        elif self._has_four():
-            self.category = 8
-            #Four of a Kind
-            self.best_five = self._get_Four_of_a_kind_cards()
-        elif self._has_flush():
-            self.category = 7
-            #Flush
-            i = len(self.flush_cards)
-            self.best_five = [card for card in self.flush_cards[i-5:i]]
-        elif self._has_fullhouse():
+        # Fullhouse and Flush categories are swapped
+        super().evaluateHand()
+        if self.category == 7:  # Fullhouse
             self.category = 6
-            #Full house
-            self.best_five = self._get_Fullhouse_cards()
-        elif self._has_straight(self.all_cards):
-            self.category = 5
-            #Straight
-        elif self._has_three():
-            self.category = 4
-            #Three of a Kind
-            self.best_five = self._get_Three_of_a_kind_cards()
-        elif self._has_two_pairs():
-            self.category = 3
-            #Two Pairs
-            self.best_five = self._get_Two_Pair_cards()
-        elif self._has_pair():
-            self.category = 2
-            #One Pair
-            self.best_five = self._get_One_Pair_cards()
-        elif self._has_high_card():
-            self.category = 1
-            #High Card
-            self.best_five = self._get_High_cards()
+        elif self.category == 6:  # Flush
+            self.category = 7
