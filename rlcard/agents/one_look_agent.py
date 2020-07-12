202a08bfc82766fe5fe4adb329fab74bb500f1a7
Joe Devlin <devlin3@pdx.edu>
2020-07-12 10:36:03 -0700

    Added test for one look agent

--- rlcard/agents/one_look_agent.py
+++ rlcard/agents/one_look_agent.py
@@ -18,20 +18,21 @@ class OneLookAgent(RandomAgent):
         '''
         self.use_raw = True
         self.action_num = action_num
-        
-    @staticmethod
-    def step(state):
+
+    @ staticmethod    
+    def getPolicy(self, actions, hand):
         ''' Based on purely the cards in the player's hand, they will be more likely to make an action.
 
         Args:
-            state (dict): An dictionary that represents the current state
+            actions (list[string]): name of actions player can take
+            hand (list[string]): Player's hole cards
 
         Returns:
-            action (int): The action predicted (randomly chosen) by the random agent
-        '''
+            probablities (list[float]): odds of choosing an action
+        '''       
         # Figureout how good the hand is just based on card ranks
         cardScore = 0
-        for card in state['raw_obs']['hand']:
+        for card in hand:
             cardScore += rank2int(card[1])
         
         # Decide play style based on our hand
@@ -41,9 +42,22 @@ class OneLookAgent(RandomAgent):
         elif cardScore < 16:
             mode = 1
         
-        # Pick an action depending on our play style
-        actions = state['raw_obs']['legal_actions']
+        # Decide play style based on hand
         weights = [MODES[mode][action] for action in actions]  
         scaler = 1/sum(weights)
-        probabilities = [p * scaler for p in weights]  
-        return np.random.choice(actions, p=probabilities)
+        return [p * scaler for p in weights] 
+
+    @staticmethod
+    def step(state):
+        ''' Player will pick a play style purely based on hards in hand
+
+        Args:
+            state (dict): An dictionary that represents the current state
+
+        Returns:
+            action (string): The action predicted (based on hand) by the random agent
+        '''
+        actions = state['raw_obs']['legal_actions']
+        hand = state['raw_obs']['hand']
+        probabilites = OneLookAgent.getPolicy(state, actions, hand)
+        return np.random.choice(actions, p=probabilites)
--- tests/agents/test_limit_holdem_one_look.py
+++ tests/agents/test_limit_holdem_one_look.py
@@ -3,11 +3,7 @@ from rlcard.agents import RandomAgent
 from rlcard.agents.one_look_agent import OneLookAgent
 from rlcard.utils import set_global_seed, tournament
 
-# Make environment
 env = rlcard.make('limit-holdem', config={'seed': 0})
-episode_num = 5
-
-# Set a global seed
 set_global_seed(0)
 
 # Set up agents
@@ -15,4 +11,7 @@ agentRando = RandomAgent(action_num=env.action_num)
 agentOneLook = OneLookAgent(action_num=env.action_num)
 env.set_agents([agentRando, agentOneLook])
 
-print(tournament(env, 1000))
+# Test
+result = tournament(env, 1000)
+print(result)
+assert(result[1] > .5)
