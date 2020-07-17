9ee4736c685c422c2881a8405529fa2979f36723
jneckar <44450838+jneckar@users.noreply.github.com>
2020-07-17 16:49:50 -0700

    Add files via upload

--- rlcard/games/shortlimitholdem/game.py
+++ rlcard/games/shortlimitholdem/game.py
@@ -1,5 +1,7 @@
 from rlcard.games.limitholdem import Game
 from rlcard.games.shortlimitholdem import Dealer
+from rlcard.games.shortlimitholdem import Judger
 
 class ShortLimitHoldemGame(Game):
     DEALER_CLASS = Dealer
+    JUDGER_CLASS = Judger
