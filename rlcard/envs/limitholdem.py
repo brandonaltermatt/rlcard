a590cf7677b1862731cec30c2849626ecfbe8a42
jake-bickle <bickle.jake@gmail.com>
2020-07-20 16:16:30 -0700

    Begin implementing LimitHoldemStateEncoder

--- rlcard/envs/limitholdem.py
+++ rlcard/envs/limitholdem.py
@@ -115,3 +115,75 @@ class LimitholdemEnv(Env):
         state['current_player'] = self.game.game_pointer
         state['legal_actions'] = self.game.get_legal_actions()
         return state
+
+class LimitHoldemStateEncoder:
+    RANK_ORDER = '23456789TJQKA'  # Allows for extension of No Limit Holdem
+
+    def __init__(self):
+        self.amount_of_ranks = len(self.RANK_ORDER)
+        self.state_size = 25 + self.amount_of_ranks * 4
+        self.state_shape = [self.state_size]
+        self.encoded_vector = None
+
+    def encode(self, state):
+        self.encoded_vector = np.zeros(self.state_size)
+        pass
+
+    def _encode_cards(self, state):
+        self._encode_card_ranks(state)
+        pass
+
+    def _encode_card_ranks(self, state):
+        def encode_card_slot(cards, starting_index):
+            for card in cards:
+                rank = card[1]
+                index = starting_index + self.RANK_ORDER.index(rank)
+                self.encoded_vector[index] = 1
+
+        hole_cards = state['hand']
+        flop_cards = sorted(state['public_cards'][:3])
+        turn_cards = state['public_cards'][3:4]
+        river_cards = state['public_cards'][4:5]
+        encode_card_slot(hole_cards, 25)
+        encode_card_slot(flop_cards, (25 + self.amount_of_ranks))
+        encode_card_slot(turn_cards, (25 + (self.amount_of_ranks * 2) + 1))
+        encode_card_slot(river_cards, (25 + (self.amount_of_ranks * 3) + 1))
+        # If there is a pair in the flop, set 1 if the pair is the higher of the two ranks
+        if flop_cards and flop_cards[1][1] != flop_cards[0][1] and flop_cards[1][1] == flop_cards[2][1]:
+            self.encoded_vector[25 + self.amount_of_ranks * 2] = 1
+
+    def _encode_card_suits(self, state):
+        hole_card_suits = [card[0] for card in state['hand']]
+        community_card_suits = [card[0] for card in state['public_cards']]
+        obs[37] = 1 if hole_card_suits[0] == hole_card_suits[1] else 0
+        suits = ['SDCH']
+        quads_suit = None
+        trips_suit = None
+        first_pair_suit = None
+        second_pair_suit = None
+        for suit in suits:
+            suit_occurances = community_card_suits.count(suit)
+            if suit_occurances == 4:
+                quads_suit = suit
+                community_card_suits = [s for s in community_card_suits if s != suit]
+            if suit_occurances == 3:
+                trips_suit = suit
+                community_card_suits = [s for s in community_card_suits if s != suit]
+            if suit_occurances == 2:
+                if first_pair_suit is None:
+                    first_pair_suit = suit
+                else:
+                    second_pair_suit = suit
+                community_card_suits = [s for s in community_card_suits if s != suit]
+        if first_pair_suit:
+            obs[38] = 1
+            obs[39] = 1 if first_pair_suit in hole_card_suits else 0
+        if second_pair_suit:
+            obs[40] = 1
+            obs[41] = 1 if second_pair_suit in hole_card_suits else 0
+        if trips_suit:
+            obs[42] = 1
+            obs[43] = 1 if trips_suit in hole_card_suits else 0
+        if quads_suit:
+            obs[44] = 1
+            obs[45] = 1 if trips_suit in hole_card_suits else 0
