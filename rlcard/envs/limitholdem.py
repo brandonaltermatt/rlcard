c48205c0be08cc8f098546941086d92e10f56855
jake-bickle <bickle.jake@gmail.com>
2020-07-21 15:48:50 -0700

    Implement LimitHoldemInfosetEncoder

--- rlcard/envs/limitholdem.py
+++ rlcard/envs/limitholdem.py
@@ -116,47 +116,61 @@ class LimitholdemEnv(Env):
         state['legal_actions'] = self.game.get_legal_actions()
         return state
 
-class LimitHoldemStateEncoder:
+class LimitHoldemInfosetEncoder:
     RANK_ORDER = '23456789TJQKA'  # Allows for extension of No Limit Holdem
 
     def __init__(self):
         self.amount_of_ranks = len(self.RANK_ORDER)
         self.state_size = 25 + self.amount_of_ranks * 4
         self.state_shape = [self.state_size]
-        self.encoded_vector = None
+        self._encoded_vector = None
 
-    def encode(self, state):
-        self.encoded_vector = np.zeros(self.state_size)
-        pass
+    def encode(self, player_state, action_record):
+        '''
+            Encodes the player state and action record into a binary list info set.
+            Refer to the following post for more information:
+            https://github.com/jake-bickle/rlcard/issues/11#issuecomment-661328769
 
-    def _encode_cards(self, state):
-        self._encode_card_ranks(state)
-        pass
+            Args:
+                player_state (dict): The dict returned by player.get_state()
+                action_record (2D array): An array of actions, where an action is an array [player_id, action_string]
 
-    def _encode_card_ranks(self, state):
-        def encode_card_slot(cards, starting_index):
+            Returns:
+                A binary list of the encoded info set
+        '''
+        self._encoded_vector = np.zeros(self.state_size)
+        self._encode_cards(player_state)
+        self._encode_bets(action_record)
+        return self._encoded_vector
+
+    def _encode_cards(self, player_state):
+        self._encode_card_ranks(player_state)
+        self._encode_card_suits(player_state)
+
+    def _encode_card_ranks(self, player_state):
+        def encode_round_ranks(cards, starting_index):
             for card in cards:
                 rank = card[1]
                 index = starting_index + self.RANK_ORDER.index(rank)
-                self.encoded_vector[index] = 1
+                if index is not None:
+                    self._encoded_vector[index] = 1
 
-        hole_cards = state['hand']
-        flop_cards = sorted(state['public_cards'][:3])
-        turn_cards = state['public_cards'][3:4]
-        river_cards = state['public_cards'][4:5]
-        encode_card_slot(hole_cards, 25)
-        encode_card_slot(flop_cards, (25 + self.amount_of_ranks))
-        encode_card_slot(turn_cards, (25 + (self.amount_of_ranks * 2) + 1))
-        encode_card_slot(river_cards, (25 + (self.amount_of_ranks * 3) + 1))
+        hole_cards = player_state['hand']
+        flop_cards = sorted(player_state['public_cards'][:3])
+        turn_cards = player_state['public_cards'][3:4]
+        river_cards = player_state['public_cards'][4:5]
+        encode_round_ranks(hole_cards, 25)
+        encode_round_ranks(flop_cards, (25 + self.amount_of_ranks))
+        encode_round_ranks(turn_cards, (25 + (self.amount_of_ranks * 2) + 1))
+        encode_round_ranks(river_cards, (25 + (self.amount_of_ranks * 3) + 1))
         # If there is a pair in the flop, set 1 if the pair is the higher of the two ranks
         if flop_cards and flop_cards[1][1] != flop_cards[0][1] and flop_cards[1][1] == flop_cards[2][1]:
-            self.encoded_vector[25 + self.amount_of_ranks * 2] = 1
+            self._encoded_vector[25 + self.amount_of_ranks * 2] = 1
 
-    def _encode_card_suits(self, state):
-        hole_card_suits = [card[0] for card in state['hand']]
-        community_card_suits = [card[0] for card in state['public_cards']]
-        obs[37] = 1 if hole_card_suits[0] == hole_card_suits[1] else 0
-        suits = ['SDCH']
+    def _encode_card_suits(self, player_state):
+        hole_card_suits = [card[0] for card in player_state['hand']]
+        community_card_suits = [card[0] for card in player_state['public_cards']]
+        suits = 'SDCH'
         quads_suit = None
         trips_suit = None
         first_pair_suit = None
@@ -175,15 +189,49 @@ class LimitHoldemStateEncoder:
                 else:
                     second_pair_suit = suit
                 community_card_suits = [s for s in community_card_suits if s != suit]
+        self._encoded_vector[0] = 1 if hole_card_suits[0] == hole_card_suits[1] else 0
         if first_pair_suit:
-            obs[38] = 1
-            obs[39] = 1 if first_pair_suit in hole_card_suits else 0
+            self._encoded_vector[1] = 1
+            self._encoded_vector[2] = 1 if first_pair_suit in hole_card_suits else 0
         if second_pair_suit:
-            obs[40] = 1
-            obs[41] = 1 if second_pair_suit in hole_card_suits else 0
+            self._encoded_vector[3] = 1
+            self._encoded_vector[4] = 1 if second_pair_suit in hole_card_suits else 0
         if trips_suit:
-            obs[42] = 1
-            obs[43] = 1 if trips_suit in hole_card_suits else 0
+            self._encoded_vector[5] = 1
+            self._encoded_vector[6] = 1 if trips_suit in hole_card_suits else 0
         if quads_suit:
-            obs[44] = 1
-            obs[45] = 1 if trips_suit in hole_card_suits else 0
+            self._encoded_vector[7] = 1
+            self._encoded_vector[8] = 1 if trips_suit in hole_card_suits else 0
+        
+    def _encode_bets(self, action_record):
+        for round_action in self._infer_actions(action_record):
+            round_number = round_action[0]
+            number_of_bets = round_action[1]
+            number_of_bets_binary = [int(x) for x in bin(number_of_bets)[2:]]  # Thanks to mgilson @ https://stackoverflow.com/a/13557953/9041692
+            player_id_of_final_action = round_action[2]
+            index_offset = 9 + (round_number * 3)
+            self._encoded_vector[index_offset] = number_of_bets_binary[0]
+            self._encoded_vector[index_offset + 1] = number_of_bets_binary[1]
+            self._encoded_vector[index_offset + 2] = player_id_of_final_action
+
+    @staticmethod
+    def _infer_actions(action_record):
+        # While action_record doesn't state when a new round of betting starts,
+        # given the 2-player structure of the game this can be inferred.
+        # Read the following post for more information:
+        # https://github.com/jake-bickle/rlcard/issues/11#issuecomment-660538937
+        round_actions = []
+        new_round = True
+        current_round = 0
+        current_amount_of_bets = 0
+        for action in action_record:
+            if action[1] == 'raise':
+                current_amount_of_bets += 1
+            if action[1] == 'check' or action[1] == 'call':
+                if new_round:
+                    new_round = False
+                else:
+                    round = [current_round, current_amount_of_bets, action[0]]
+                    current_round += 1
+                    current_amount_of_bets = 0
+        return round_actions
