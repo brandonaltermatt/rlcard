e0aae531d7bbd3c63e6e0971c7d6b5dbe6391e37
jake-bickle <bickle.jake@gmail.com>
2020-07-22 18:02:03 -0700

    LimitHoldemEnv utilizes LimitHoldemInfosetEncoder

--- rlcard/envs/limitholdem.py
+++ rlcard/envs/limitholdem.py
@@ -6,116 +6,6 @@ import rlcard
 from rlcard.envs import Env
 from rlcard.games.limitholdem import Game
 
-class LimitholdemEnv(Env):
-    ''' Limitholdem Environment
-    '''
-    
-    # Allow setting a custom Game class. This makes it easier to
-    # extend limitholdem, such as in shortlimitholdem.
-    GAME_CLASS = Game
-
-    def __init__(self, config):
-        ''' Initialize the Limitholdem environment
-        '''
-        self.name = 'limit-holdem'
-        self.game = Game()
-        super().__init__(config)
-        self.actions = ['call', 'raise', 'fold', 'check']
-        self.state_shape=[72]
-
-        with open(os.path.join(rlcard.__path__[0], 'games/limitholdem/card2index.json'), 'r') as file:
-            self.card2index = json.load(file)
-
-    def _get_legal_actions(self):
-        ''' Get all leagal actions
-
-        Returns:
-            encoded_action_list (list): return encoded legal action list (from str to int)
-        '''
-        return self.game.get_legal_actions()
-
-    def _extract_state(self, state):
-        ''' Extract the state representation from state dictionary for agent
-
-        Note: Currently the use the hand cards and the public cards. TODO: encode the states
-
-        Args:
-            state (dict): Original state from the game
-
-        Returns:
-            observation (list): combine the player's score and dealer's observable score for observation
-        '''
-        extracted_state = {}
-
-        legal_actions = [self.actions.index(a) for a in state['legal_actions']]
-        extracted_state['legal_actions'] = legal_actions
-
-        public_cards = state['public_cards']
-        hand = state['hand']
-        raise_nums = state['raise_nums']
-        cards = public_cards + hand
-        idx = [self.card2index[card] for card in cards]
-        obs = np.zeros(72)
-        obs[idx] = 1
-        for i, num in enumerate(raise_nums):
-            obs[52 + i * 5 + num] = 1
-        extracted_state['obs'] = obs
-
-        if self.allow_raw_data:
-            extracted_state['raw_obs'] = state
-            extracted_state['raw_legal_actions'] = [a for a in state['legal_actions']]
-        if self.record_action:
-            extracted_state['action_record'] = self.action_recorder
-        return extracted_state
-
-    def get_payoffs(self):
-        ''' Get the payoff of a game
-
-        Returns:
-           payoffs (list): list of payoffs
-        '''
-        return self.game.get_payoffs()
-
-    def _decode_action(self, action_id):
-        ''' Decode the action for applying to the game
-
-        Args:
-            action id (int): action id
-
-        Returns:
-            action (str): action for the game
-        '''
-        legal_actions = self.game.get_legal_actions()
-        if self.actions[action_id] not in legal_actions:
-            if 'check' in legal_actions:
-                return 'check'
-            else:
-                return 'fold'
-        return self.actions[action_id]
-
-    def _load_model(self):
-        ''' Load pretrained/rule model
-
-        Returns:
-            model (Model): A Model object
-        '''
-        from rlcard import models
-        return models.load('limit-holdem-rule-v1')
-
-    def get_perfect_information(self):
-        ''' Get the perfect information of the current state
-
-        Returns:
-            (dict): A dictionary of all the perfect information of the current state
-        '''
-        state = {}
-        state['chips'] = [self.game.players[i].in_chips for i in range(self.player_num)]
-        state['public_card'] = [c.get_index() for c in self.game.public_cards] if self.game.public_cards else None
-        state['hand_cards'] = [[c.get_index() for c in self.game.players[i].hand] for i in range(self.player_num)]
-        state['current_player'] = self.game.game_pointer
-        state['legal_actions'] = self.game.get_legal_actions()
-        return state
-
 class LimitHoldemInfosetEncoder:
     RANK_ORDER = '23456789TJQKA'  # Allows for extension of No Limit Holdem
 
@@ -144,6 +34,8 @@ class LimitHoldemInfosetEncoder:
         return self._encoded_vector
 
     def _encode_cards(self, player_state):
+        if not player_state['hand']:
+            return
         self._encode_card_ranks(player_state)
         self._encode_card_suits(player_state)
 
@@ -175,7 +67,6 @@ class LimitHoldemInfosetEncoder:
         for suit in suits:
             suit_occurances = community_card_suits.count(suit)
             if suit_occurances == 4:
-                quads_suit = suit
                 self._encoded_vector[7] = 1
                 self._encoded_vector[8] = 1 if suit in hole_card_suits else 0
             if suit_occurances == 3:
@@ -231,3 +122,97 @@ class LimitHoldemInfosetEncoder:
                 new_round = True
                 round_number += 1
         return round_actions
+
+class LimitholdemEnv(Env):
+    ''' Limitholdem Environment
+    '''
+    ENCODER = LimitHoldemInfosetEncoder()
+    GAME_CLASS = Game
+
+    def __init__(self, config):
+        ''' Initialize the Limitholdem environment
+        '''
+        self.name = 'limit-holdem'
+        self.game = self.GAME_CLASS()
+        config['record_action'] = True  # The encoder requires details on bets
+        super().__init__(config)
+        self.actions = ['call', 'raise', 'fold', 'check']
+        self.state_shape = self.ENCODER.state_shape
+
+    def _get_legal_actions(self):
+        ''' Get all leagal actions
+
+        Returns:
+            encoded_action_list (list): return encoded legal action list (from str to int)
+        '''
+        return self.game.get_legal_actions()
+
+    def _extract_state(self, state):
+        ''' Extract the state representation from state dictionary for agent
+
+        Args:
+            state (dict): Original state from the game
+
+        Returns:
+            observation (list): combine the player's score and dealer's observable score for observation
+        '''
+        extracted_state = {}
+
+        legal_actions = [self.actions.index(a) for a in state['legal_actions']]
+        extracted_state['legal_actions'] = legal_actions
+        extracted_state['obs'] = self.ENCODER.encode(state, self.action_recorder)
+
+        if self.allow_raw_data:
+            extracted_state['raw_obs'] = state
+            extracted_state['raw_legal_actions'] = [a for a in state['legal_actions']]
+        if self.record_action:
+            extracted_state['action_record'] = self.action_recorder
+        return extracted_state
+
+    def get_payoffs(self):
+        ''' Get the payoff of a game
+
+        Returns:
+           payoffs (list): list of payoffs
+        '''
+        return self.game.get_payoffs()
+
+    def _decode_action(self, action_id):
+        ''' Decode the action for applying to the game
+
+        Args:
+            action id (int): action id
+
+        Returns:
+            action (str): action for the game
+        '''
+        legal_actions = self.game.get_legal_actions()
+        if self.actions[action_id] not in legal_actions:
+            if 'check' in legal_actions:
+                return 'check'
+            else:
+                return 'fold'
+        return self.actions[action_id]
+
+    def _load_model(self):
+        ''' Load pretrained/rule model
+
+        Returns:
+            model (Model): A Model object
+        '''
+        from rlcard import models
+        return models.load('limit-holdem-rule-v1')
+
+    def get_perfect_information(self):
+        ''' Get the perfect information of the current state
+
+        Returns:
+            (dict): A dictionary of all the perfect information of the current state
+        '''
+        state = {}
+        state['chips'] = [self.game.players[i].in_chips for i in range(self.player_num)]
+        state['public_card'] = [c.get_index() for c in self.game.public_cards] if self.game.public_cards else None
+        state['hand_cards'] = [[c.get_index() for c in self.game.players[i].hand] for i in range(self.player_num)]
+        state['current_player'] = self.game.game_pointer
+        state['legal_actions'] = self.game.get_legal_actions()
+        return state
