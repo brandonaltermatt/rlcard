ffb5e7408039d804a448d259f108d9243a3bd677
jake-bickle <bickle.jake@gmail.com>
2020-08-01 13:28:43 -0700

    Move LimitHoldem infoset encoders to their own file, _limitholdem_infoset_encoders.py

--- rlcard/envs/shortlimitholdem.py
+++ rlcard/envs/shortlimitholdem.py
@@ -1,10 +1,11 @@
-from rlcard.envs.limitholdem import LimitholdemEnv, LimitHoldemInfosetEncoder, MiniEncoder
+from rlcard.envs.limitholdem import LimitholdemEnv
+from rlcard.envs._limitholdem_infoset_encoders import LimitHoldemInfosetEncoder, NoFlushEncoder
 from rlcard.games.shortlimitholdem import Game
 
 class ShortLimitHoldemInfosetEncoder(LimitHoldemInfosetEncoder):
     RANK_ORDER = '6789TJQKA'
     
-class MiniShortEncoder(MiniEncoder):
+class MiniShortEncoder(NoFlushEncoder):
     RANK_ORDER = '6789TJQKA'    
 
 class ShortlimitholdemEnv(LimitholdemEnv):
